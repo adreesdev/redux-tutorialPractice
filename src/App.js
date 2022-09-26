0d0fe544b78141d8f1fc407b6ac93b83f192c577
Muhammad Idrees <adreesdev@gmail.com>
2022-09-26 21:47:17 +0500

    v1

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+
+const initialState = {
+	cart: cartItems,
+	total: 0,
+	amount: 0,
+};
+
+const store = createStore(
+	reducer,
+	initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+function App() {
+	// cart setup
+
+	return (
+		<Provider store={store}>
+			<Navbar />
+			<CartContainer />
+		</Provider>
+	);
+}
+
+export default App;
